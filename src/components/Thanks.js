d5b6553e273d8fba6bd1933efad35ca87e1dd374
mcvholloway <mcvholloway@gmail.com>
2024-01-26 11:17:09 -0600

    Add thanks you section

---
+++ src/components/Thanks.js
@@ -0,0 +1,66 @@
+function Thanks() {
+  return (
+    <section className="section is-medium" id="thanks">
+            <div className="container content">
+
+        <h2 className="title has-text-centered">Thank You!</h2>
+
+        <div class="container">
+            <p className="is-family-secondary"><b>thank-you (noun / ˈthaŋk-ˌyü ): </b>a polite expression of one's gratitude</p>
+
+            <p className="is-family-secondary">Merriam-Webster’s definition of “Thank You” and our own written words fall woefully short in describing the level of appreciation and gratitude we feel and wish to express for the support and mentorship received while participating in the Nashville Software School (NSS) Part-Time Data Analytics Cohort – DA8. However, please accept our sincere appreciation.</p>
+
+              <h5 class="thank-you-title">To the founder and CEO, <b>John Wark</b>:</h5>
+
+              <p className="is-family-secondary">It is with sincerest appreciation that we say “Thank You” for the vision to build a community that embraces technology, diversity, inclusion, and opportunity for all. Your hard work and dedication to this school and its programs will leave a lasting impression on Nashville’s technical community and our individual lives.</p>
+              
+              <h5 class="thank-you-title">To our instructors - , <b>Amanda Partlow</b>, <b>Jonathon Hankins</b>, and <b>Rob Schulteis</b>:</h5>
+
+              <p className="is-family-secondary">We say “Thank You” for leading us in developing the critical skills required to become data professionals. From our initial start in spreadsheets through SQL, Python, and Capstone projects, the challenges we experienced as individuals and as a group were not few. The lessons we have learned both personally and professionally will guide us as we embark on this new chapter in life. Your tireless efforts and expert facilitation set the proper conditions for success in this course and our future careers.</p>
+              
+              <h5 class="thank-you-title">To the faculty and staff, <b>Jeremey Bakker</b> and <b>Michael Holloway</b>:</h5>
+              
+              <p className="is-family-secondary">We say “Thank You” for enabling our attendance in this course and delivering a data analytics program that is second to none. NSS has truly been an environment that enabled us to leverage our aptitude, motivation, and drive to enter the technical workforce and build a better life for ourselves and our families.</p>
+
+              <h5 class="thank-you-title">To Career Development, <b>Michael Frieh</b> and <b>Ashley Canino</b>:</h5>
+
+              <p className="is-family-secondary">We say “Thank You” for preparing us in one of the most challenging aspects of the transition to the technical workforce - finding and securing employment with the right company. Your care, consideration, and commitment to our success were evident in every one-on-one discussion, class presentation, interview preparation session, and resume or LinkedIn profile review. We could not have been placed in better hands to prepare us for the highly competitive job market.</p>
+
+              <h5 class="thank-you-title">To the data professionals and partners:</h5>
+
+              <p className="is-family-secondary">Finally, we say “Thank You” to the numerous data professionals listed below who took time from their busy lives and schedules to share their knowledge and experience with our cohort. From introducing us to Tableau and Power Bi to Geospatial Analysis to Lunch and Learns to Roundtable Events to Mock Interviews and supporting our Capstone presentations, this community of professionals has welcomed us to the data community and opened the door to becoming their colleagues. We look forward to the opportunity of “Paying it Forward” to future cohorts as we continue to develop ourselves as data professionals.</p>
+
+              <ul class="flex-list">
+                      <div class="item"><li>Deme Khamken</li></div>
+                      <div class="item"><li>Ben Hummel</li></div>
+                      <div class="item"><li>Jen Whitson</li></div>
+                      <div class="item"><li>Lesley Easley</li></div>
+                      <div class="item"><li>Tito Amoguis</li></div>
+                      <div class="item"><li>Daniel Spaulding</li></div>
+                      <div class="item"><li>Ann Rumsey</li></div>
+                      <div class="item"><li>Cody Asbury</li></div>
+                      <div class="item"><li>Ed Pearson</li></div>
+                      <div class="item"><li>Caleb Autry</li></div>
+                      <div class="item"><li>Jasmine Drumright</li></div>
+                      <div class="item"><li>Keith Goodwin</li></div>
+                      <div class="item"><li>Ed Marx</li></div>
+                      <div class="item"><li>Mylah Gainey</li></div>
+                      <div class="item"><li>Ella S Fayer</li></div>
+                      <div class="item"><li>Zoe Teets</li></div>
+                      <div class="item"><li>Nadia Marie Roumanos</li></div>
+                      <div class="item"><li>Abi Inglis</li></div>
+                      <div class="item"><li>Matt Rodgers</li></div>
+                      <div class="item"><li>Andrea Busch</li></div>
+                      <div class="item"><li>Chris Hart</li></div>
+                      <div class="item"><li>Ed Pearson</li></div>
+                      <div class="item"><li>Elise Holmes</li></div>
+                      <div class="item"><li>Shannon Pargin</li></div>
+                      <div class="item"><li>Katie Garrett</li></div>
+              </ul>
+          </div>
+          </div>
+        </section>
+  )
+}
+
+export default Thanks;
